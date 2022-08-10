01760bd71579e6e36fe16e108bdb51ac9e364248
zarasah <zarinasah1989@gmail.com>
2022-08-10 22:50:47 +0400

    change env file

--- index.js
+++ index.js
@@ -16,13 +16,14 @@ app.use(json);
 app.use(cookieParser());
 app.use('/v1', indexRouter);
 
-// (async () => {
-//     await mongoose.connect(process.env.DB_URL);
-// })();
-
 (async () => {
-    await mongoose.connect('mongodb+srv://zara:zara@finalprojectszal.hgnplxa.mongodb.net/userproject?retryWrites=true&w=majority');
+    await mongoose.connect(process.env.DB_URL);
 })();
+
+console.log(process.env.DB_URL);
+// (async () => {
+//     await mongoose.connect('mongodb+srv://zara:zara@finalprojectszal.hgnplxa.mongodb.net/userproject?retryWrites=true&w=majority');
+// })();
 
 http.listen(port, () => {
     console.log(`server is listening port ${port}`);
