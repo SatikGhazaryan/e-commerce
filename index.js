45e0b90264bbe19128209c31da965a527c9c75ea
zarasah <zarinasah1989@gmail.com>
2022-08-10 15:24:54 +0400

    change static path

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const json = express.json();
 const port = process.env.PORT || 3003;
 dotenv.config();
 
-const staticFiles = express.static('./front/user/user.html');
+const staticFiles = express.static('./front');
 app.use(staticFiles);
 
 app.use(json);
